a0c4e675ab631296b43bbfc44a035de548378395
loesel19 <aloesel2@gmail.com>
2022-07-24 16:53:18 -0400

    changed the way the database was implemented so that connections are closed immediately after database transactions are finished. fixed the deleteEntry JS function in report page so that it throws the proper entryID to the controller.

--- budgetApp/Controllers/HomeController.cs
+++ budgetApp/Controllers/HomeController.cs
@@ -7,6 +7,15 @@ using System.Text;
 
 namespace budgetApp.Controllers
 {
+    /* AUTHOR : Andrew A. Loesel
+     * Organization : None. This is a personal project :)
+     * Purpose : This controller will service the home (*at this point only*) directory for my budgetapp ?budgetIO?
+     *           The purpose of this application is to replace spreadsheets (even though I will still probably use them)
+     *           as a means to monitor my income/ spending. This application allows for account creation/ signing, creating
+     *           expense/ income entrys which can be sorted by categories, and customizable report viewing, ***and at some point
+     *           I would like to implement a feature for downloading a csv of the reports***.
+     * Connected Services : This app uses github for VCS. It is pipelined to Azure for deployment. Lastly, a Heroku postgres sql server
+     *                      is used for cloud hosted data persistence. */
     public class HomeController : Controller
     {
         //add our config
@@ -18,6 +27,7 @@ namespace budgetApp.Controllers
         [HttpGet]
         public IActionResult Index()
         {
+            /* This is the HTTPGet method for our index page. The way I implemented this
             /* check if the username is populated to ensure that user is signed in */
             if (String.IsNullOrEmpty(GlobalVariables.GlobalUsername))
             {
@@ -28,7 +38,20 @@ namespace budgetApp.Controllers
                     GlobalVariables.GlobalUsername = username;
                     /* we also want to get the userID for this user from the pgsql table. */
                     string strSQL = "SELECT * FROM users Where Username = '" + username + "';";
-                    NpgsqlDataReader reader = clsDatabase.ExecuteDataReader(strSQL, config.GetValue<string>("DBConnString"));
+                    /******* since we use postgres sql here we have to have an air of caution around our transactions. Postgres
+                     * can be fussy about what transactions it allows, so for some instances (like deletion) we will need to encapsulate those
+                     * transactions with a npgsqlTransaction object. For reading and inserting it will be fine to just make an instance of our database class
+                     * and execute our sql... hopefully. */
+                    //make an instance of our database object
+                    clsDatabase objDB = new clsDatabase(config.GetValue<string>("DBConnString"));
+                    //try to open the connection
+                    if (!objDB.openConnection())
+                    {
+                        //connection failed. here we should throw an exception since we let down the user this early no point in trying to front about it
+                        objDB.Dispose(); //dispose of our disappointment of an object in case the user is gracious enough to give the application another try
+                        throw new NpgsqlException("connection with database failed");
+                    }
+                    NpgsqlDataReader reader = objDB.ExecuteDataReader(strSQL);
                     try
                     {
                         if (reader.Read())
@@ -37,10 +60,18 @@ namespace budgetApp.Controllers
                         }
                     } catch (Exception ex)
                     {
+                        //note we did not check if reader was null, that will be caught here
+                        //we want to close any open objects like the reader and connection
                         reader.Close();
+                        objDB.closeConnection();
+                        //now we can dispose of objDB as well
+                        objDB.Dispose();
                         return RedirectToAction("SignIn");
                     }
+                    //clean up objects and dispose of objDB
                     reader.Close();
+                    objDB.closeConnection();
+                    objDB.Dispose();
                     return View();
                 }catch(Exception ex)
                 {
@@ -80,8 +111,19 @@ namespace budgetApp.Controllers
             string sqlCommand = String.Format("INSERT INTO entrys (amount, category, subcategory, description, userID, createdtime) values({0}, {1}, {4}, {2}, {3}, {5});",
                 "'" + model.amount + "'", "'" + model.category + "'", "'" + model.description + "'", "'" + GlobalVariables.UserID + "'", "'" + model.subCategory + "'", 
                 "'" + insertDate + "'");
-            //now try to insert the column
-            if (clsDatabase.ExecuteSQLNonQuery(sqlCommand, config.GetValue<string>("DBConnString")))
+
+            //make an instance of our db class
+            clsDatabase objDB = new clsDatabase(config.GetValue<string>("DBConnString"));
+            //try to open the connection
+            if (!objDB.openConnection())
+            {
+                //send a message to the GUI, and dispose of the dud object
+                ViewBag.Message = "Could not open database connection.";
+                objDB.Dispose();
+                return View();
+            }
+            //now try to insert the new row
+            if (objDB.ExecuteSQLNonQuery(sqlCommand))
             {
                 /* we were able to add the entry to our database */
                 ViewBag.Message = "Successfully added entry.";
@@ -91,7 +133,9 @@ namespace budgetApp.Controllers
                 ViewBag.Message = "Something went wrong, entry not posted.";
 
             }
-            /* after posting we just return the index view */
+            //now cleanup open connection and dispose of our database object
+            objDB.closeConnection();
+            objDB.Dispose();
             return View();
         }
 
@@ -149,7 +193,19 @@ namespace budgetApp.Controllers
                     break;
             }
             sbSQL.Append(" ORDER BY CreatedTime DESC;");
-            NpgsqlDataReader sdr = clsDatabase.ExecuteDataReader(sbSQL.ToString(), config.GetValue<string>("DBConnString"));
+            //make an instance of our db class.
+            clsDatabase objDB = new clsDatabase(config.GetValue<string>("DBConnString"));
+            //see if we can open the connection
+            if (!objDB.openConnection())
+            {
+                //rats. notify the user that something went wrong opening the connection through a GUI message.
+                ViewBag.Message = "Unable to open Database connection.";
+                objDB.Dispose(); //cleanup the DB object so that no funny business happens if a new one is made.
+                //return the view
+                return View();
+            }
+            //create a data reader based off the query we built.
+            NpgsqlDataReader sdr = objDB.ExecuteDataReader(sbSQL.ToString());
             /* lets start building our table, put in the headers first. Space them nicely as well */
             StringBuilder strTable = new StringBuilder("<table class=\"table\" id=\"reportTable\" style=\"table-layout: fixed; border-collapes: collapse;\">");
             strTable.AppendLine("   <tr>");
@@ -211,21 +267,36 @@ namespace budgetApp.Controllers
                 //increment counter
                 count++;
             }
-            //dont forget to close the table
+            //dont forget to close the table tag
             strTable.AppendLine("</table>");
             //now we need to set the totals as well
             model.strHTML = "<table><tr><th class=\"th-md\">Total Spent</th>" +
                 "<td>&nbsp$" + spent + "</td>" + "<th class=\"th-md\">Total Net</th>" +
                 "<td>&nbsp$" + (income - spent) + "</td></tr></table>";
             model.strHTMLTable = strTable.ToString();
+            //clean up loose ends with the datareader, connection and lastly the object.
             sdr.Close();
+            objDB.closeConnection();
+            objDB.Dispose();
             return View(model);
         }
         [HttpPost]
         public IActionResult SignIn(UserModel model)
         {
             string strSQL = "SELECT userID, username FROM users WHERE username = '" + model.username + "' AND password = '" + model.password + "';";
-            NpgsqlDataReader reader = clsDatabase.ExecuteDataReader(strSQL, config.GetValue<string>("DBConnString"));
+            //make an instance of our db class.
+            clsDatabase objDB = new clsDatabase(config.GetValue<string>("DBConnString"));
+            //try to open the connection
+            if (!objDB.openConnection())
+            {
+                //mission failed. let the user know what a disgrace this application is with a GUI message
+                ViewBag.Msg = "Could not open database Connection.";
+                //dispose of our failure, just to so that no one will see it ever again
+                objDB.Dispose();
+                //give the user something to look at
+                return View();
+            }
+            NpgsqlDataReader reader = objDB.ExecuteDataReader(strSQL);
             if(!(reader == null))
             {
                 if (reader.Read())
@@ -240,35 +311,51 @@ namespace budgetApp.Controllers
                     {
                         //something wrong with the data
                         ViewBag.Msg = "Account error.";
+                        //close the loose connection and reader
+                        reader.Close();
+                        objDB.closeConnection();
+                        //dispose of the objDB instance
+                        objDB.Dispose();
                         return View();
                     }
+                    //before we serve the webpage we need to close our reader, connection, and dispose of the objDB instance
+                    reader.Close();
+                    objDB.closeConnection();
+                    objDB.Dispose();
                     return RedirectToAction("Index");
                 }
+                //theoretically this point shouldn't be reached, but better safe than sorry.
                 //incorrect signin values
                 ViewBag.Msg = "Username/password incorrect.";
+                //before we serve the webpage we need to close our reader, connection, and dispose of the objDB instance
+                reader.Close();
+                objDB.closeConnection();
+                objDB.Dispose(); 
                 return View();
             }
             else
             {
                 //incorrect signin values
                 ViewBag.Msg = "Username/password incorrect.";
+                //we need to close our reader, connection, and dispose of the objDB instance
+                reader.Close();
+                objDB.closeConnection();
+                objDB.Dispose();
                 return View();
             }
-            /* TODO: check database that username passowrd combo is correct */
-            //if correct
             
-            //if wrong
-            //return View(false);
         }
         public IActionResult SignOut()
         {
+            /** This method 'signs the user out' right now we just delete the cookie that is stored in the browser,
+             * eventually we will have to deal with whatever authentication method is used. */
             GlobalVariables.GlobalUsername = null;
             GlobalVariables.UserID = -1;
             /* we will also want to 'delete' the username cookie */
             try
             {
                 Response.Cookies.Delete("username");
-            } catch (Exception ex) { }
+            } catch (Exception ex) { throw ex; }
 
             return RedirectToAction("Index");
         }
@@ -276,6 +363,8 @@ namespace budgetApp.Controllers
         [HttpGet]
         public IActionResult signUp()
         {
+            /**
+             * This is the HTTPGet method for the signup page. We just return the page to the client */
             return View();
         }
         [HttpPost]
@@ -283,29 +372,59 @@ namespace budgetApp.Controllers
         {
             /* data validated in view, but need to make sure the username is unique */
             string strSQL = "SELECT * FROM users WHERE username = '" + model.username + "';";
-            NpgsqlDataReader sdr = clsDatabase.ExecuteDataReader(strSQL, config.GetValue<string>("DBConnString"));
+            // make an instance of our db class
+            clsDatabase objDB = new clsDatabase(config.GetValue<string>("DBConnString"));
+            //try to open up a connection
+            if (!objDB.openConnection())
+            {
+                //I am running out of things to say for these. just dispose of the database object and get it out of my face
+                objDB.Dispose();
+                //oh and letting the user know what happened would be smart. Then we can return the page to them as well
+                ViewBag.Msg = "Could not connect to the database.";
+                return View();
+            }
+            NpgsqlDataReader sdr = objDB.ExecuteDataReader(strSQL);
             if (sdr == null)
             {
-                ViewBag.Msg = "There was an error with the database connection.";
+                /* Note that we handle the null case here specifacally. If there are no users with this username
+                * already in the database we would get an empty dataReader, so something else happened either in
+                * the code or with the database. */ 
+                ViewBag.Msg = "An error occured with the database.";
+                //close down the reader, connection and dispose of the database object
+                sdr.Close();
+                objDB.closeConnection();
+                objDB.Dispose();
                 return View();
             }
             if (sdr.Read())
             {
                 ViewBag.Msg = "Username already taken.";
+                //close down the reader, connection and dispose of the database object
+                sdr.Close();
+                objDB.closeConnection();
+                objDB.Dispose();
                 return View();
             }
             else
             {
                 /* we can sign up the new user */
                 strSQL = "INSERT INTO users (username, password) values ('" + model.username + "', '" + model.password + "');";
-                if(clsDatabase.ExecuteSQLNonQuery(strSQL, config.GetValue<string>("DBConnString"))){
+                if(objDB.ExecuteSQLNonQuery(strSQL)){
                     /* user was signed up successfully */
                     ViewBag.Msg = "Account created succesfully please signin.";
+                    //close down the reader, connection and dispose of the database object
+                    sdr.Close();
+                    objDB.closeConnection();
+                    objDB.Dispose();
                     return RedirectToAction("SignIn");
                 }
                 else
                 {
                     ViewBag.Msg = "Something went wrong creating your account, please try again.";
+                    //close down the reader, connection and dispose of the database object
+                    sdr.Close();
+                    objDB.closeConnection();
+                    objDB.Dispose();
                     return View();
                 }
             }
@@ -313,14 +432,60 @@ namespace budgetApp.Controllers
         public bool deleteEntry([FromQuery] int entryID)
         {
             /* this method will be accessed through an ajax call in the report page. The user wants to delete a row from the database 
-             * so we */
-            string stringSql = "DELETE From entrys WHERE entryID = " + entryID + ";";
-            if(clsDatabase.ExecuteSQLNonQuery(stringSql, config.GetValue<string>("DBConnString")))
+             * here we will have to encapsulate our sql commands in a npgsqlTransaction. */
+            //make an instance of our db object
+            clsDatabase objDB = new clsDatabase(config.GetValue<string>("DBConnString"));
+
+            //lets get a connection from our database class
+            NpgsqlConnection conn = objDB.getConnection();
+            //and try to open that connection
+            try
             {
-                return true;
+                conn.Open();
+            }catch (Exception ex)
+            {
+                ViewBag.Msg = "could not open database connection. Deletion has been aborted.";
+                return false;
             }
-            else
+            try
             {
+                /* npgsql is actually quite fussy about what you can do. so we need to
+                 * delete from a transaction clause instead of through a straight up query, or
+                 * else it will seem like our changes were made, but the database will not commit that deletion. */
+                using (NpgsqlTransaction tr = conn.BeginTransaction())
+                {
+                    string stringSql = "DELETE From entrys WHERE entryID = " + entryID + ";";
+                    //execute the sql statement
+                    try
+                    {
+                        NpgsqlCommand cmd = new NpgsqlCommand(stringSql, conn);
+                        cmd.Transaction = tr;
+                        cmd.ExecuteNonQuery();
+                        //commit the changes to the db
+                        tr.Commit();
+                        //close connection
+                        conn.Close();
+                        //dispose of the database object
+                        objDB.Dispose();
+                        //close the transaction as well, just to be safe. I do not trust it :\
+                        tr.Dispose();
+                        //return true to the ajax function this is called from
+                        return true;
+                    }catch (Exception ex)
+                    {
+                        //the delete did not work, rollback any changes made to our db
+                        tr.Rollback();
+                        //close conn, dispose db object and return false
+                        conn.Close();
+                        
+                        objDB.Dispose();
+                        return false;
+                    }
+                    
+                }
+            }catch (Exception ex)
+            {
+                //just return false here
                 return false;
             }
         }
--- budgetApp/clsDatabase.cs
+++ budgetApp/clsDatabase.cs
@@ -4,43 +4,109 @@ using Microsoft.Extensions.Options;
 using Npgsql;
 namespace budgetApp
 {
-    public static class clsDatabase
+    public class clsDatabase
     { 
-       
-        public static Boolean ExecuteSQLNonQuery(string strSQL, string connString)
+        private NpgsqlConnection _connection; //our connection with the database
+        private bool _connected = false;
+        /* The following methods are more rudementary setup methods. First is a parameterized constructor and the next 2 are 
+         * control methods for the database. Note that we will need to open and close the connection everytime we want to do
+         * something with an instance of this class in the controller. After implementing this as a static class I did some 
+         * researching and thinking and think that for this instance it is better implement this as a nonstatic class, and 
+         * make sure to clean up after ourselves in the controller, leaving no loose connections open */
+        public clsDatabase(string strConn)
         {
-            //get connection to server
-            
-            NpgsqlConnection conn = new NpgsqlConnection(connString);
-            NpgsqlCommand cmd = new NpgsqlCommand(strSQL, conn);
+            /* parameterized constructor, here we will take in the connection string to connect to our database. an 
+             * assign field _connection to a new npgsqlConnection with that connection string. The reason we do not
+             * do this in a default constructor is because we grab that conn string from the config, which can only be
+             * accessed in the controller. */
+            _connection = new NpgsqlConnection(strConn);
+        }
+        public bool openConnection()
+        {
+            /* here we just want to open the connection that should have received its connection string when the object was 
+             * constructed. We return true if the connection was sucessfully opened otherwise we return false. */
             try
             {
-                conn.Open();
-
+                _connection.Open();
+                //set our connected boolean to true
+                _connected = true;
+                return true;
+            } catch (Exception ex)
+            {
+                return false;
+            }
+        }
+        public bool closeConnection()
+        {
+            /* all this method will do is close the connection, and return true if it succeeds in that. Otherwise return false */
+            try
+            {
+                _connection.Close();
+                //set the connected boolean to false
+                _connected = false;
+                return true;
             }catch (Exception ex)
             {
                 return false;
             }
+        }
+        public NpgsqlConnection getConnection()
+        {
+            /* this method will return our connection. It is needed for when we use transactions. */
+            return _connection;
+        }
+        /* the following method is a dispose method */
+        public void Dispose()
+        {
+            /* by calling dispose here we will clean up any hanging connections or other loose ends within the class. 
+             * This method should be called everytime we finish using the clsDatabase object to keep our application
+             * running smoothing on this end. */
+            _connection.Dispose();
+            //after we 'manually dispose' unmanaged rescources we can suppress the finalization of the object so that garbage collection does
+            // not try to do it again.
+            GC.SuppressFinalize(this);
+        }
+       // The following subprograms are used to interact with data in our database
+        public Boolean ExecuteSQLNonQuery(string strSQL)
+        {
+            /* This method will be used to execute any npgsql commands that do not provide results. 
+             * if the command is executed we will return true, and return false if an exception occurrs
+             * so that we can handle it in our controller and update the GUI accordingly. */
+
+            //first lets check if we are connected to the database.
+            if (!_connected)
+            {
+                //we can not do anything without a connection, so return false.
+                return false;
+            }
+            //create a new npgsql command to execute our nonquery
+            NpgsqlCommand cmd = new NpgsqlCommand(strSQL, _connection);
+            //now try to execute it and return true on success, and false on failure.
             try
             {
                 cmd.ExecuteNonQuery();
-                conn.Close();
                 return true;
             }
             catch(Exception ex)
             {
-                conn.Close();
                 return false;
             }
         }
-        public static NpgsqlDataReader ExecuteDataReader(string strSQL, string connString)
+        public NpgsqlDataReader ExecuteDataReader(string strSQL)
         {
-            NpgsqlConnection conn = new NpgsqlConnection(connString);
-            NpgsqlCommand cmd = new NpgsqlCommand(strSQL, conn);
+            //see if the connection is opened
+            if (!_connected)
+            {
+                //return null here since without an open connection we cannot get a data reader
+                return null;
+            }
+            //make a new command
+            NpgsqlCommand cmd = new NpgsqlCommand(strSQL, _connection);
+            //try to execute our command and return the results in a reader.
             try
             {
-                conn.Open();
-                return cmd.ExecuteReader();
+                NpgsqlDataReader reader = cmd.ExecuteReader();
+                return reader;
             }catch(Exception ex)
             {
                 return null;
